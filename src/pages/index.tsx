b0ae852a970e87db4851ec96fbdd5e65f79a100d
benbaessler <mail@benbaessler.com>
2022-10-21 17:37:05 +0100

    Created assets container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,37 +12,48 @@ const Home: NextPage = () => {
   const valueLabelFormat = (value: number) => `${value}%`
 
   return (
-    <div className={styles.container}>
-
-      <div className={styles.row}>
-        <span>
-          <h1>Borrow</h1>
-        </span>
-        <span style={{ justifyContent: 'center' }}>
-          <Button variant="text" startIcon={<VisibilityOff/>} className={styles.button}>
-            USD Value
-          </Button>
-        </span>
-        <span style={{ justifyContent: 'flex-end' }}>
-          <IconButton aria-label="help" className={styles.button}>
-            <InfoOutlined fontSize="small"/>
-          </IconButton>
-        </span>
+    <div className={styles.container} style={{
+      width: '500px',
+      height: '700px',
+      justifyContent: 'space-between'
+    }}>
+      <div>
+        <div className={styles.row}>
+          <span>
+            <h1>Borrow</h1>
+          </span>
+          <span style={{ justifyContent: 'center' }}>
+            <Button variant="text" startIcon={<VisibilityOff/>} className={styles.button}>
+              USD Value
+            </Button>
+          </span>
+          <span style={{ justifyContent: 'flex-end' }}>
+            <IconButton aria-label="help" className={styles.button}>
+              <InfoOutlined fontSize="small"/>
+            </IconButton>
+          </span>
+        </div>
       </div>
 
-      <div className={styles.row}>
-        <h3 style={{ marginRight: '10px' }}>Borrow Capacity</h3>
-        <IconButton aria-label="help" className={styles.button} style={{ marginRight: '5px' }}>
-          <InfoOutlined fontSize="small"/>
-        </IconButton>
-        <CustomSlider
-          disabled
-          track={false}
-          value={Number(Number(MOCK_DATA.borrowCapacity).toFixed(3))}
-          valueLabelDisplay="on"
-          getAriaValueText={valueLabelFormat}
-          valueLabelFormat={valueLabelFormat}
-        />
+      <div>
+        <div className={styles.row} style={{ marginBottom: '10px' }}>
+          <h3 style={{ marginRight: '10px' }}>Borrow Capacity</h3>
+          <IconButton aria-label="help" className={styles.button} style={{ marginRight: '5px' }}>
+            <InfoOutlined fontSize="small"/>
+          </IconButton>
+          <CustomSlider
+            disabled
+            track={false}
+            value={Number(Number(MOCK_DATA.borrowCapacity).toFixed(3))}
+            valueLabelDisplay="on"
+            getAriaValueText={valueLabelFormat}
+            valueLabelFormat={valueLabelFormat}
+          />
+        </div>
+
+        <div className={styles.container} style={{ minHeight: '200px' }}>
+          
+        </div>
       </div>
 
     </div>
